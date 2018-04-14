26bb30b8002552ae83a8e9044df3b82bffc2f178
George Valtas <grvaltas@gmail.com>
2018-04-14 15:56:04 +0300

    updates

--- lcg scripts/periodicity/periodicity_of_the_lcg.js
+++ lcg scripts/periodicity/periodicity_of_the_lcg.js
@@ -1,3 +1,13 @@
+/*
+
+Author: George Valtas
+
+A script that find the period lenght
+of a linear congruential generator.
+
+*/
+
+
 // Xn = aXn-1 + c (mod m)
 
 var a = 11
@@ -14,15 +24,18 @@ for ( i = 1; i < 1000; i++ ) {
 console.log(X)
 
 if ( X.includes(57) ) {
-	/*var newX = X.filter(function(e) {
-		return e !== 57
-	})*/
-	//debug('new array: ' + newX)
+	// var index takes the random number we want to look for in the series
 	var index = X.indexOf(57);
 	if ( index !== -1 ) X.splice(index, 1)
+	/* 
+	if a number in the series is equal to 57
+	then we want to find its index in the array
+	and so we use the findIndex() method
+	*/
 	function period(e) { 
 		return e == 57
 	}
-	console.log(X.findIndex(period))
-	console.log(X[101])
+	console.log(X.findIndex(period)) // output: 101
+	// to check if the 101th number is 57 we try this:
+	console.log(X[101]) // output: 57 
 }
