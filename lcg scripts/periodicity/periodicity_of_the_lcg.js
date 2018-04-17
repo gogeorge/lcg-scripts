b05293925dd0c48bc972d79a3389dc1295118b69
George Valtas <grvaltas@gmail.com>
2018-04-17 10:41:11 +0300

    Update periodicity_of_the_lcg.js

--- lcg scripts/periodicity/periodicity_of_the_lcg.js
+++ lcg scripts/periodicity/periodicity_of_the_lcg.js
@@ -35,6 +35,12 @@ if ( X.includes(57) ) {
 	function period(e) { 
 		return e == 57
 	}
+	/*
+	In math article, the output is 103 instead of 101.
+	Mathematically it is 103 but here it is 101 for the two reasons:
+	- arrays are zero inxeded, so the "0th" number is not considered (so 103 becomes 102)
+	- i removed the seed number (57) so i could make this program work (so 102 becomes 101) 
+	*/
 	console.log(X.findIndex(period)) // output: 101
 	// to check if the 101th number is 57 we try this:
 	console.log(X[101]) // output: 57 
